7bb80fed55cc79f7ec539f0fe7c8e20a0168f03a
Heggy Castaneda <heggyy@gmail.com>
2021-07-20 10:45:13 -0400

    check if I could get to api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ class App extends Component {
     super()
   }
 
+  componentDidMount() {
+    // fetch api request for data
+    // wait until component has mounted on virtual DOM
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(result => console.log(result))
+  }
+
   render() {
+    // we don't set state inside of render since state keeps getting updated over and over.
     return <h1>Hello API!</h1>
   }
 }
