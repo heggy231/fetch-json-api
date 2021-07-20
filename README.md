3c8439a7e812827526e24cc8a6da5a1d606e98c0
Heggy Castaneda <heggyy@gmail.com>
2021-07-20 11:31:44 -0400

    create separate function for fetching api

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 [JSONPlaceholder](https://jsonplaceholder.typicode.com/)
 
+- data format of photo api data: 
+(https://jsonplaceholder.typicode.com/photos)
+```js
+[ 
+  {
+    albumId: 1
+    id: 1
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+    title: "accusamus beatae ad facilis cum similique qui sunt"
+    url: "https://via.placeholder.com/600/92c952"
+  }
+]
+
+```
+
 ```js
 // fetch template to follow
 fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -19,5 +34,16 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(result => console.log(result))
   }
-  
+
+```
+
+- slice img to only show 50 images.
+
+```js
+var list = [1, 2, 3, 4, 5]
+list.slice(0, 3)
+// => [1, 2, 3]
+
+var size = 50; // first 50 img
+var photoItems = this.state.photos.slice(0, size).map(...)
 ```
--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-
   constructor() {
-    super()
+    super();
+
+    // store the api data inside my state
+    this.state = {
+      photos: [],
+    };
   }
 
   componentDidMount() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos = () => {
     // fetch api request for data
     // wait until component has mounted on virtual DOM
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(response => response.json())
-      .then(result => console.log(result))
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({
+          // array of data [{}, {}, {}]
+          photos: result,
+        });
+      });
   }
 
   render() {
     // we don't set state inside of render since state keeps getting updated over and over.
-    return <h1>Hello API!</h1>
+    const size = 15;
+    const photoItems = this.state.photos.slice(0, size).map((photo) => {
+      return (
+        <li key={photo.id}>
+          <label>{photo.title}</label>
+          <img src={photo.thumbnailUrl} alt="" />
+        </li>
+      );
+    });
+
+    return <ul>{photoItems}</ul>;
   }
 }
 
